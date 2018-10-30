e50ab1b2b1fb64d1ade65d16be6355d0c62453cf
stehnoc <chris.stehno@netscout.com>
2018-10-30 09:29:16 -0500

    scans the local repo and builds coords

--- source/app.d
+++ source/app.d
@@ -1,6 +1,76 @@
 import std.stdio;
+import std.file;
+import std.path;
+import std.algorithm: canFind;
+import std.string;
+import std.getopt;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+struct Dependency {
+    string group;
+    string artifact;
+    string vers;
+    string classifier;
+    string type;
 }
+
+void main(string[] args) {
+    string localRepo = "";
+    string remoteRepo = "";
+
+    auto helpInfo = getopt(
+        args,
+        "local", "Path to the local repo.", &localRepo,
+        "remote", "URL for the remote repo.", &remoteRepo
+    );
+
+    if(!localRepo.empty){
+        writeln("Scanning ", localRepo, "...");
+
+        int prefixLen = localRepo.length;
+        int resolved = 0;
+
+        foreach (string path; dirEntries(localRepo, SpanMode.depth)){
+            if (isFile(path) && canFind([".pom", ".jar" ], extension(path))){
+                auto artifactPath = path[prefixLen+1..$];
+
+                Dependency dependency = parseDependency(artifactPath);
+
+                writeln(artifactPath, " --> ", dependency);
+                resolved++;
+            }
+        }
+
+        writeln("=========================================");
+        writeln("Resvoled: " , resolved, " artifacts.");
+
+    } else {
+        defaultGetoptPrinter("A tool for resolving differences between local and remote repos.",helpInfo.options);
+    }
+}
+
+private Dependency parseDependency(string artifactPath){
+    auto ext = extension(artifactPath);
+    auto parts = split(artifactPath, "\\");
+
+    string artifactGroup = join(parts[0..($-3)], "/");
+    string artifactName = parts[$-3];
+
+    string artifactVersion = parts[$-2];
+    string artifactFile = parts[$-1];
+
+    string classifier = null;
+    if ( count(artifactFile, "-") > 2 ){
+        auto start = artifactFile.lastIndexOf("-") + 1;
+        auto end = artifactFile.lastIndexOf(".");
+        classifier = artifactFile[start..end];
+
+        artifactVersion = replace(artifactVersion, "-" ~ classifier, "");
+    }
+
+    return Dependency(artifactGroup, artifactName, artifactVersion, classifier, ext[1..$]);
+}
+
+/*
+    group:artfiact:version:type:classifier
+    ROOT/GROUP/ARTIFACT/VERSION/ARTIFACT-VERSION-CLASSIFIER.TYPE
+ */
